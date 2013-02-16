bcb2d3c3b305315619bbab7a8b5d92d3426a021e
Enrico Marino <enrico.marino@email.com>
2013-02-16 17:13:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require('./lib/ns.js');
+
+/**
+ * globalize
+ * globalize component
+ * 
+ * @copyright 2013 Enrico Marino
+ * @license MIT
+ */
+
+/**
+ * Expose `gloabalize`
+ */
+
+exports.globalize = globalize;
+
+/** 
+ * globalize
+ * globalize the properties of `obj`
+ * 
+ * @param {Object} obj properties to globalize
+ * @api public
+ */
+
+function globalize (obj) {
+  for (var property in obj) {
+    exports[property] = obj[property]
+  }
+}
